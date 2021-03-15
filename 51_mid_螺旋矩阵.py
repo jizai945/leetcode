73b7c7d12c4fab40a7568c1bee74f20c3819dd00
Timo <738859736@qq.com>
2021-03-16 00:01:01 +0800

    feat: 提交了54
    Signed-off-by: Timo <738859736@qq.com>

---
+++ 51_mid_螺旋矩阵.py
@@ -0,0 +1,56 @@
+# 54. 螺旋矩阵
+# 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+
+ 
+
+# 示例 1：
+
+
+# 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# 输出：[1,2,3,6,9,8,7,4,5]
+# 示例 2：
+
+
+# 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+# 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+
+# 提示：
+
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 10
+# -100 <= matrix[i][j] <= 100
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix or not matrix[0]: return []
+        M, N = len(matrix), len(matrix[0])
+        left, right, up, down = 0, N - 1, 0, M - 1
+        res = []
+        x, y = 0, 0
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        cur_d = 0
+        while len(res) != M * N:
+            res.append(matrix[x][y])
+            if cur_d == 0 and y == right:
+                cur_d += 1
+                up += 1
+            elif cur_d == 1 and x == down:
+                cur_d += 1
+                right -= 1
+            elif cur_d == 2 and y == left:
+                cur_d += 1
+                down -= 1
+            elif cur_d == 3 and x == up:
+                cur_d += 1
+                left += 1
+            cur_d %= 4
+            x += dirs[cur_d][0]
+            y += dirs[cur_d][1]
+        return res
