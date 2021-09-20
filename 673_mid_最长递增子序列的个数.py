9dd6ea229100abd4db99512f4a08ac8d427e9d1a
Timo <738859736@qq.com>
2021-09-20 17:27:23 +0800

    add 673

---
+++ 673_mid_最长递增子序列的个数.py
@@ -0,0 +1,38 @@
+# 673. 最长递增子序列的个数
+# 给定一个未排序的整数数组，找到最长递增子序列的个数。
+
+# 示例 1:
+
+# 输入: [1,3,5,4,7]
+# 输出: 2
+# 解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+# 示例 2:
+
+# 输入: [2,2,2,2,2]
+# 输出: 5
+# 解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+# 注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
+
+# 通过次数37,358提交次数90,076
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        n, max_len, ans = len(nums), 0, 0
+        dp = [0] * n
+        cnt = [0] * n
+        for i, x in enumerate(nums):
+            dp[i] = 1
+            cnt[i] = 1
+            for j in range(i):
+                if x > nums[j]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        cnt[i] = cnt[j]  # 重置计数
+                    elif dp[j] + 1 == dp[i]:
+                        cnt[i] += cnt[j]
+            if dp[i] > max_len:
+                max_len = dp[i]
+                ans = cnt[i]  # 重置计数
+            elif dp[i] == max_len:
+                ans += cnt[i]
+        return ans
