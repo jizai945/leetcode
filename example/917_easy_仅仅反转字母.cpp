36345f148108fd47bdb5f1ae3904e7872e6a0b4a
jizai945 <738859736@qq.com>
2022-02-23 21:01:58 +0800

    add 917

---
+++ example/917_easy_仅仅反转字母.cpp
@@ -0,0 +1,55 @@
+// 917. 仅仅反转字母
+// 给你一个字符串 s ，根据下述规则反转字符串：
+
+// 所有非英文字母保留在原有位置。
+// 所有英文字母（小写或大写）位置反转。
+// 返回反转后的 s 。
+
+ 
+
+// 示例 1：
+
+// 输入：s = "ab-cd"
+// 输出："dc-ba"
+// 示例 2：
+
+// 输入：s = "a-bC-dEf-ghIj"
+// 输出："j-Ih-gfE-dCba"
+// 示例 3：
+
+// 输入：s = "Test1ng-Leet=code-Q!"
+// 输出："Qedo1ct-eeLg=ntse-T!"
+ 
+
+// 提示
+
+// 1 <= s.length <= 100
+// s 仅由 ASCII 值在范围 [33, 122] 的字符组成
+// s 不含 '\"' 或 '\\'
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        string ans;
+        stack<char> sk;
+        
+        for (auto ch: s) {
+            if ( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ) {
+                sk.push(ch);
+            }
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+            if ( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ) {
+                ch = sk.top();
+                ans += ch;
+                sk.pop();
+            } else {
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
