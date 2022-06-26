a14e8b96f4820b3635d5ed802567fe9bc4c92740
jizai945 <738859736@qq.com>
2022-06-27 01:11:33 +0800

    add 522

---
+++ example/522_mid_最长特殊序列 II.cpp
@@ -0,0 +1,57 @@
+// 522. 最长特殊序列 II
+// 给定字符串列表 strs ，返回其中 最长的特殊序列 。如果最长特殊序列不存在，返回 -1 。
+
+// 特殊序列 定义如下：该序列为某字符串 独有的子序列（即不能是其他字符串的子序列）。
+
+//  s 的 子序列可以通过删去字符串 s 中的某些字符实现。
+
+// 例如，"abc" 是 "aebdc" 的子序列，因为您可以删除"aebdc"中的下划线字符来得到 "abc" 。"aebdc"的子序列还包括"aebdc"、 "aeb" 和 "" (空字符串)。
+ 
+
+// 示例 1：
+
+// 输入: strs = ["aba","cdc","eae"]
+// 输出: 3
+// 示例 2:
+
+// 输入: strs = ["aaa","aaa","aa"]
+// 输出: -1
+ 
+
+// 提示:
+
+// 2 <= strs.length <= 50
+// 1 <= strs[i].length <= 10
+// strs[i] 只包含小写英文字母
+
+class Solution {
+public:
+    int findLUSlength(vector<string>& strs) {
+        auto is_subseq = [](const string& s, const string& t) -> bool {
+            int pt_s = 0, pt_t = 0;
+            while (pt_s < s.size() && pt_t < t.size()) {
+                if (s[pt_s] == t[pt_t]) {
+                    ++pt_s;
+                }
+                ++pt_t;
+            }
+            return pt_s == s.size();
+        };
+
+        int n = strs.size();
+        int ans = -1;
+        for (int i = 0; i < n; ++i) {
+            bool check = true;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && is_subseq(strs[i], strs[j])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                ans = max(ans, static_cast<int>(strs[i].size()));
+            }
+        }
+        return ans;
+    }
+};
